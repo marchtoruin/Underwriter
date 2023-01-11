2236cfb4ea9cc4391046bb51b7d74c45221ceafe
marchtoruin <justin.j.mcclure@hotmail.com>
2023-01-10 18:50:56 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+function countOnes(num) {
+    let binary = num.toString(2);  // convert number to binary
+    let count = 0;
+    for (let i = 0; i < binary.length; i++) {
+      if (binary[i] === "1") {
+        count++;
+      }
+    }
+    console.log("Number of 1's: " + count);
+  }
+  
+  countOnes(15698);  // prints "Number of 1's: 2"
+  
+
+  const myName = "Justin";
+  const myStr = "My name is " + myName + " and I am well!";
+  console.log(myStr);
+  
+
+  const someAdjective = "bad";
+  let myStr = "Learning to code is ";
+  myStr += someAdjective;
+  console.log(myStr);
+
+  
+
+  let lastNameLength = 0;
+  const lastName = "Lovelace";
+  lastNameLength = lastName.length;
+  console.log(lastNameLength);    //console outputs the length of the string value.
+  
+
+  let firstLetterOfLastName = "";
+  const lastName = "Lovelace";
+
+  firstLetterOfLastName = lastName[0];
+  console.log(firstLetterOfLastName); //Bracket notation is a way to get a character at a specific index within a string.
+                                           //console outputs "L" in this case.
+*/
+
+
+
